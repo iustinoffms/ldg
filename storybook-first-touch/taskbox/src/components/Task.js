ee4908044a07ab569df180fd7ddd957d75ad9fae
Iustin Sanduleanu <iustin.sanduleanu@thinslices.com>
2022-09-01 16:39:35 +0300

    add blue background

--- storybook-first-touch/taskbox/src/components/Task.js
+++ storybook-first-touch/taskbox/src/components/Task.js
@@ -35,7 +35,7 @@ export default function Task({
           readOnly={true}
           name="title"
           placeholder="Input title"
-          style={{ background: "red" }}
+          style={{ background: "blue" }}
         />
       </label>
 
